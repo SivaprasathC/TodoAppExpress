d0adc24f5fafb38f76fb67d287d7a9fc9081e8e3
SivaprasathC <168990425+SivaprasathC@users.noreply.github.com>
2025-02-04 02:01:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// text='<div id="todos"><p>hello</p></div>'
+// document.getElementById("todolist").innerHTML = text
+
+
+fetch("https://appsail-50024778614.development.catalystappsail.in/todos")
+.then(response=> response.json())
+.then(res=>{
+    const data=res;
+    let todo='';
+    data.forEach(list => {
+       todo+=`<div id="todos"><p>${list.todo}</p><h5 style="color: red;">Deadline: ${list.deadline}</h5><button style="height:40px;width:100px;border-radius: 15px;background-color: red;color: azure;" id="${list._id}"  onclick="remove(this.id)">Delete</button></div>`
+
+    })
+    document.getElementById('todolist').innerHTML=todo;
+})
+
+function remove(id){
+    fetch('https://appsail-50024778614.development.catalystappsail.in/delete/' + id, {
+        method: 'DELETE',
+      })
+      .then(res => window.alert("Deleted successfully"))
+}
