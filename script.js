15cd5fae314e9d2c04839a339129e192b34fdf86
Siva Prasath <sivaprasathchellamuthu@gmail.com>
2025-02-04 02:36:10 +0530

    ui changes but need to add create and edit and need to solve cors with delete and footer and firebase auth needed

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ fetch("https://appsail-50024778614.development.catalystappsail.in/todos")
     const data=res;
     let todo='';
     data.forEach(list => {
-       todo+=`<div id="todos"><p>${list.todo}</p><h5 style="color: red;">Deadline: ${list.deadline}</h5><button style="height:40px;width:100px;border-radius: 15px;background-color: red;color: azure;" id="${list._id}"  onclick="remove(this.id)">Delete</button></div>`
-
+       todo+=` <div id="todos">
+                <p>${list.todo}</p>
+                <h5 style="color: red;">Deadline: ${list.deadline}</h5>
+                <div id="buttons">
+                    <button style="height:40px;width:100px;border-radius: 15px;background-color: red;color: azure;font-size: large;" id="${list._id} "  onclick="remove(this.id)">Delete</button>
+                    <button style="height:40px;width:100px;border-radius: 15px;background-color: rgb(28, 170, 76);color: azure;font-size: large;" id="/update/${list._id} " onclick="remove(this.id)">Edit</button>
+                </div>
+            </div>`
     })
     document.getElementById('todolist').innerHTML=todo;
 })
