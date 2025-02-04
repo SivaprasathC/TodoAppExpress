c8e48a4b858500100dae252b7f02c74d6ed02507
Siva Prasath <sivaprasathchellamuthu@gmail.com>
2025-02-04 19:03:09 +0530

    solving cors

--- script.js
+++ script.js
@@ -13,16 +13,21 @@ fetch("https://appsail-50024778614.development.catalystappsail.in/todos")
                 <h5 style="color: red;">Deadline: ${list.deadline}</h5>
                 <div id="buttons">
                     <button style="height:40px;width:100px;border-radius: 15px;background-color: red;color: azure;font-size: large;" id="${list._id} "  onclick="remove(this.id)">Delete</button>
-                    <button style="height:40px;width:100px;border-radius: 15px;background-color: rgb(28, 170, 76);color: azure;font-size: large;" id="/update/${list._id} " onclick="remove(this.id)">Edit</button>
+                    <button style="height:40px;width:100px;border-radius: 15px;background-color: rgb(28, 170, 76);color: azure;font-size: large;" id="/update/${list._id} " onclick="remove(this.id)" class="edit">Edit</button>
                 </div>
             </div>`
     })
     document.getElementById('todolist').innerHTML=todo;
 })
 
-function remove(id){
+function remove(id) {
     fetch('https://appsail-50024778614.development.catalystappsail.in/delete/' + id, {
         method: 'DELETE',
-      })
-      .then(res => window.alert("Deleted successfully"))
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        mode: 'cors'  // Ensure the request mode is set to 'cors'
+    })
+    .then(() => window.alert("Deleted successfully"))
+    .catch(error => console.error("Error:", error));
 }
