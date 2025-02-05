90be6331b505572d9282b362d4259cef3ab8e3ab
SivaprasathC <sivaprasathchellamuthu@gmail.com>
2025-02-06 01:00:06 +0530

    made all changes

--- script.js
+++ script.js
@@ -20,13 +20,57 @@ fetch("https://appsail-50024778614.development.catalystappsail.in/todos")
     document.getElementById('todolist').innerHTML=todo;
 })
 
+
 function remove(id) {
     fetch('https://appsail-50024778614.development.catalystappsail.in/delete/' + id, {
         method: 'DELETE',
     })
-    .then((function(){window.alert("Deleted successfully")
-                   location.reload(true)
+    .then((function()
+    {   document.getElementById('operation').innerHTML=`<h3 style="color: red;">Todo Deleted Successfully &#9989;</h3>`
+        setTimeout(() => {
+            window.location.reload();
+        }, 1350);
     }))
     .catch(error => console.error("Error:", error)); 
      
+}
+
+
+function create(){
+
+    let todo=document.getElementById('input').value;
+    //todo example rcvd- 2025-02-11T00:35 to 11-02-2025 0:35
+    let deadline=document.getElementById('inputdate').value;
+    const datetimearray=deadline.split('T'); //splitting date and time
+    let time=datetimearray[1]; //time
+    let date=datetimearray[0]; //date
+    let datearray=date.split('-'); //splitting date into day,month,year
+    let day=datearray[2];
+    let month=datearray[1];
+    let year=datearray[0];
+    let formatteddate=`${day}-${month}-${year} ${time}`;
+
+
+    if(todo.trim()!='')
+    {
+        let data={
+            "todo":todo.trim(),
+            "deadline":formatteddate
+        }
+        fetch("https://appsail-50024778614.development.catalystappsail.in/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(data), //covert to json string
+        })
+        .then((function()
+        {   document.getElementById('operation').innerHTML=`<h3 style="color: green;">Todo Added Successfully &#9989;</h3>`
+            setTimeout(() => {
+                window.location.reload();
+            }, 1350);
+        }))
+        .catch(error => console.error("Error:", error)); 
+    }
+   
 }
