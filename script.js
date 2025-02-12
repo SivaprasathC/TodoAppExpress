4c40d609ecb079d815730822da7a76e06a2d68ac
SivaprasathC <sivaprasathchellamuthu@gmail.com>
2025-02-13 00:26:16 +0530

    UI change

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ fetch("https://appsail-50024778614.development.catalystappsail.in/todos")
                 <p>${list.todo}</p>
                 <h5 style="color: red;">Deadline: ${list.deadline}</h5>
                 <div id="buttons">
-                    <button style="height:40px;width:100px;border-radius: 15px;background-color: red;color: azure;font-size: large;" id="${list._id} "  onclick="remove(this.id)">Delete</button>
-                    <button style="height:40px;width:100px;border-radius: 15px;background-color: rgb(28, 170, 76);color: azure;font-size: large;" id="/update/${list._id}" class="edit" onclick="edit(this.id)">Edit</button>
+                    <button style="height:40px;width:100px;border-radius: 30px;background-color: red;color: azure;font-size: large;padding-bottom:6px" id="${list._id} "  onclick="remove(this.id)">Delete</button>
+                    <button style="height:40px;width:100px;border-radius: 30px;background-color: rgb(28, 170, 76);color: azure;font-size: large;padding-bottom:6px" id="/update/${list._id}" class="edit" onclick="edit(this.id)">Edit</button>
                 </div>
             </div>`
     })
