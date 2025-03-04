91bb09c3d11732cb769fd9bf94a0f4924b026026
SivaprasathC <sivaprasathchellamuthu@gmail.com>
2025-03-05 00:27:41 +0530

    remove dummy express file

--- Express Server/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const connectDB = async () => {
-   await mongoose.connect(process.env.URI).then((res)=>{  // aync await is used because mongoose.connect() runs asynchronously(it returns a promise), so console.log("Connected to MongoDB") may execute before the connection is actually established.
-         console.log("Connected to MongoDB");
-   } )
-}
-
-export default connectDB; //when exporting only single function, use export default
